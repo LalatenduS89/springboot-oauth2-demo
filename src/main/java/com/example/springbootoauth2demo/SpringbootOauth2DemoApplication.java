5e84232ffd1ada53f4c24274102dc228d6c6def1
lsethi <lsethi@LSETHI-IN.oradev.oraclecorp.com>
2020-02-23 12:48:21 +0530

    second commit

--- src/main/java/com/example/springbootoauth2demo/SpringbootOauth2DemoApplication.java
+++ src/main/java/com/example/springbootoauth2demo/SpringbootOauth2DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.springbootoauth2demo;
 
 import java.security.Principal;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
@@ -14,9 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringbootOauth2DemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringbootOauth2DemoApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		System.out.println("Inside init method");
+	}
+	
 	@GetMapping("/")
 	public String getMessage(Principal principal) {
 		return "Hi, " + principal.getName() + " Welcome to OAuth2 Application";
